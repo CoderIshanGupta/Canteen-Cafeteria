5d00ac487bfa3f0ee204c502713154ddcede1b73
CoderIshanGupta <guptaishan2609@gmail.com>
2025-05-24 22:10:40 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,48 @@
+import 'package:canteen_cafeteria/providers/orders_provider.dart';
+import 'package:canteen_cafeteria/screens/cart_screen.dart';
+import 'package:canteen_cafeteria/screens/order_history_screen.dart';
+import 'package:canteen_cafeteria/screens/orders_screen.dart';
+import 'package:canteen_cafeteria/screens/payment_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/home_screen.dart';
-import 'screens/cart_screen.dart';
-import 'screens/orders_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/auth_screen.dart';
+import 'providers/user_provider.dart';
 import 'providers/cart_provider.dart';
-import 'providers/orders_provider.dart';
 
 void main() {
-  runApp(CafeteriaApp());
-}
-
-class CafeteriaApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
+  runApp(
+    MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
         ChangeNotifierProvider(create: (_) => CartProvider()),
         ChangeNotifierProvider(create: (_) => OrdersProvider()),
       ],
-      child: MaterialApp(
-        title: 'College Cafeteria',
-        theme: ThemeData(primarySwatch: Colors.orange),
-        initialRoute: '/',
-        routes: {
-          '/': (ctx) => HomeScreen(),
-          '/cart': (ctx) => CartScreen(),
-          '/orders': (ctx) => OrdersScreen(),
-        },
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'College Cafeteria',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
       ),
+      initialRoute: '/auth',
+      routes: {
+        '/auth': (_) => AuthScreen(),
+        '/home': (_) => HomeScreen(),
+        '/profile': (_) => ProfileScreen(),
+        '/orders': (_) => OrdersScreen(),
+        '/cart': (_) => CartScreen(),
+        '/payment': (_) => PaymentScreen(),
+        '/order-history': (_) => OrderHistoryScreen(),
+      },
     );
   }
 }
