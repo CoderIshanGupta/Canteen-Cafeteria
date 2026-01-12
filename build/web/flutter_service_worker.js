962c9d46426e048c435cecd0971424f6ab72827b
CoderIshanGupta <guptaishan2609@gmail.com>
2026-01-12 16:47:01 +0530

    Add prebuilt Flutter Web for Vercel deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -50,7 +50,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d2eedadae31aca0f9dfb187ffc1859d4
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "39fdcd4c82b83f9513285409af61d106",
+"flutter_bootstrap.js": "55291c011f96a827f1094701ee198f65",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
